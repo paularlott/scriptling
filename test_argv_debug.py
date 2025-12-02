46e4c3d51448c7f80924783cdfbe0bf926e1c534
Paul <paul@the7thdream.com>
2025-12-02 08:21:07 +0800

    updated tests

--- test_argv_debug.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-print("Full argv:", sys.argv)
-print("Length:", len(sys.argv))
-for i, arg in enumerate(sys.argv):
-    print(f"argv[{i}] = {arg}")
--- test_import_as.py
+++
@@ -1,4 +0,0 @@
-import sys as s
-
-print("Script name:", s.argv[0])
-print("Args:", s.argv[1:])
