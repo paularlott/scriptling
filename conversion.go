d668fa9256304c892566b3ae8125b29ae9ecce55
Paul <paul@the7thdream.com>
2025-12-30 00:19:10 +0800

    Helper functions

---
+++ conversion.go
@@ -0,0 +1,131 @@
+package scriptling
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/paularlott/scriptling/object"
+)
+
+// FromGo converts a Go interface{} value to a scriptling Object.
+// It handles primitive types (nil, bool, int, float, string), nested structures
+// (maps, slices), and falls back to JSON marshaling for unknown types.
+func FromGo(v interface{}) object.Object {
+	switch v := v.(type) {
+	case nil:
+		return &object.Null{}
+	case bool:
+		return &object.Boolean{Value: v}
+	case int:
+		return object.NewInteger(int64(v))
+	case int8:
+		return object.NewInteger(int64(v))
+	case int16:
+		return object.NewInteger(int64(v))
+	case int32:
+		return object.NewInteger(int64(v))
+	case int64:
+		return object.NewInteger(v)
+	case uint:
+		return object.NewInteger(int64(v))
+	case uint8:
+		return object.NewInteger(int64(v))
+	case uint16:
+		return object.NewInteger(int64(v))
+	case uint32:
+		return object.NewInteger(int64(v))
+	case uint64:
+		// Note: May overflow for very large uint64 values
+		return object.NewInteger(int64(v))
+	case float32:
+		return &object.Float{Value: float64(v)}
+	case float64:
+		return &object.Float{Value: v}
+	case string:
+		return &object.String{Value: v}
+	case []interface{}:
+		elements := make([]object.Object, len(v))
+		for i, item := range v {
+			elements[i] = FromGo(item)
+		}
+		return &object.List{Elements: elements}
+	case map[string]interface{}:
+		pairs := make(map[string]object.DictPair)
+		for key, val := range v {
+			pairs[key] = object.DictPair{
+				Key:   &object.String{Value: key},
+				Value: FromGo(val),
+			}
+		}
+		return &object.Dict{Pairs: pairs}
+	default:
+		// For unknown types, try to convert to JSON then parse
+		jsonBytes, err := json.Marshal(v)
+		if err != nil {
+			return &object.String{Value: fmt.Sprintf("%v", v)}
+		}
+		var result interface{}
+		if err := json.Unmarshal(jsonBytes, &result); err != nil {
+			return &object.String{Value: fmt.Sprintf("%v", v)}
+		}
+		return FromGo(result)
+	}
+}
+
+// ToGo converts a scriptling Object to a Go interface{}.
+// It returns the underlying Go value for the object type.
+func ToGo(obj object.Object) interface{} {
+	if obj == nil {
+		return nil
+	}
+
+	switch o := obj.(type) {
+	case *object.Null:
+		return nil
+	case *object.Boolean:
+		return o.Value
+	case *object.Integer:
+		return o.Value
+	case *object.Float:
+		return o.Value
+	case *object.String:
+		return o.Value
+	case *object.List:
+		result := make([]interface{}, len(o.Elements))
+		for i, elem := range o.Elements {
+			result[i] = ToGo(elem)
+		}
+		return result
+	case *object.Tuple:
+		result := make([]interface{}, len(o.Elements))
+		for i, elem := range o.Elements {
+			result[i] = ToGo(elem)
+		}
+		return result
+	case *object.Dict:
+		result := make(map[string]interface{})
+		for _, pair := range o.Pairs {
+			// Keys can be any Object, but typically are strings
+			if keyStr, ok := pair.Key.(*object.String); ok {
+				result[keyStr.Value] = ToGo(pair.Value)
+			} else {
+				// For non-string keys, use Inspect() representation
+				result[pair.Key.Inspect()] = ToGo(pair.Value)
+			}
+		}
+		return result
+	case *object.Error:
+		return o.Message
+	case *object.Builtin:
+		// Return help text if available, otherwise generic string
+		if o.HelpText != "" {
+			return o.HelpText
+		}
+		return "<builtin function>"
+	case *object.Function:
+		return o.Name
+	default:
+		// For other types (like ReturnValue, Break, Continue), return string representation
+		return o.Inspect()
+	}
+}
