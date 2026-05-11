3decf1e7630a98a40d267ae2ce6ec5d496db4173
Paul <paul@the7thdream.com>
2026-05-11 23:16:40 +0800

    Expand tests

--- evaluator/perf_regression_test.go
+++ evaluator/perf_regression_test.go
@@ -90,3 +90,54 @@ result
 	evaluated := testEval(input)
 	testIntegerObject(t, evaluated, 8)
 }
+
+func TestLambdaClosureCaptureWorksWithCallEnvReuse(t *testing.T) {
+	input := `
+def make_adder(x):
+    return lambda y: x + y
+
+add_five = make_adder(5)
+add_ten = make_adder(10)
+result1 = add_five(3)
+result2 = add_ten(3)
+result3 = add_five(7)
+[result1, result2, result3]
+`
+
+	evaluated := testEval(input)
+	list, ok := evaluated.(*object.List)
+	if !ok {
+		t.Fatalf("object is not List. got=%T (%+v)", evaluated, evaluated)
+	}
+	if len(list.Elements) != 3 {
+		t.Fatalf("list has wrong length. got=%d, want=3", len(list.Elements))
+	}
+	testIntegerObject(t, list.Elements[0], 8)
+	testIntegerObject(t, list.Elements[1], 13)
+	testIntegerObject(t, list.Elements[2], 12)
+}
+
+func TestLambdaClosureCaptureDifferentValues(t *testing.T) {
+	input := `
+fns = []
+for i in range(5):
+    fns.append(lambda x: x + i)
+
+results = []
+for fn in fns:
+    results.append(fn(10))
+results
+`
+
+	evaluated := testEval(input)
+	list, ok := evaluated.(*object.List)
+	if !ok {
+		t.Fatalf("object is not List. got=%T (%+v)", evaluated, evaluated)
+	}
+	if len(list.Elements) != 5 {
+		t.Fatalf("list has wrong length. got=%d, want=5", len(list.Elements))
+	}
+	for _, elem := range list.Elements {
+		testIntegerObject(t, elem, 14)
+	}
+}
